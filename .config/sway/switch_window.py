978d6ce30570cd63bf0c5307b58fcef271bca797
Esa Varemo <esa@kuivanto.fi>
2019-06-23 00:30:14 +0300

    Clean up / comment the window switcher

--- .config/sway/switch_window.py
+++ .config/sway/switch_window.py
@@ -3,9 +3,11 @@
 import subprocess
 import json
 
-tree = json.loads(subprocess.check_output(['swaymsg', '-t', 'get_tree']))
 
 def handle_container(con):
+    """
+    Recursively find all windows
+    """
 
     windows = []
 
@@ -21,8 +23,11 @@ def handle_container(con):
 
     return windows
 
+
+tree = json.loads(subprocess.check_output(['swaymsg', '-t', 'get_tree']))
 windows = []
 
+# Find all workspaces and the windows in them
 for output in tree['nodes']:
     for workspace in output['nodes']:
         if 'nodes' not in workspace.keys():
@@ -31,9 +36,10 @@ for output in tree['nodes']:
         for container in workspace['nodes']:
             windows = windows + handle_container(container)
 
+# Format the list of windows for dmenu/rofi
 windows_string = '\n'.join([f"<{app_id}> {app_name} --- {app_title}" for app_id, app_name, app_title in windows])
 
-
+# Call rofi and move focus to the selected window
 selection = subprocess.check_output(['rofi', '-dmenu', '-i'], input=windows_string, universal_newlines=True)
 window_id = selection.split(' ')[0][1:-1]
-subprocess.check_output(['swaymsg', f"[con_id=\"{window_id}\"]", 'focus'])
+subprocess.call(['swaymsg', f"[con_id=\"{window_id}\"]", 'focus'])

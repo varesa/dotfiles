ea81740da7f4f835c44029b55ec2fb9f1dce1aac
Esa Varemo <esa@kuivanto.fi>
2019-06-23 00:15:51 +0300

    Sway configs (work in progress)

---
+++ .config/sway/switch_window.py
@@ -0,0 +1,42 @@
+#!/bin/env python3
+
+import subprocess
+import json
+
+tree = json.loads(subprocess.check_output(['swaymsg', '-t', 'get_tree']))
+
+def handle_container(con):
+    #print(con['name'])
+
+    windows = []
+
+    if 'app_id' in con.keys():
+        app_id = con['id']
+        app_name = con['app_id'] if con['app_id'] else con['window_properties']['class']
+        app_title = con['name']
+
+        #print(f"<{app_id}> {app_name} --- {app_title}")
+        windows.append((app_id, app_name, app_title,))
+
+    for child in con['nodes']:
+        windows = windows + handle_container(child)
+
+    return windows
+
+windows = []
+
+for output in tree['nodes']:
+    for workspace in output['nodes']:
+        if 'nodes' not in workspace.keys():
+            continue
+
+        for container in workspace['nodes']:
+            windows = windows + handle_container(container)
+
+windows_string = '\n'.join([f"<{app_id}> {app_name} --- {app_title}" for app_id, app_name, app_title in windows])
+
+#print(windows_string)
+
+selection = subprocess.check_output(['dmenu', '-i'], input=windows_string, universal_newlines=True)
+window_id = selection.split(' ')[0][1:-1]
+subprocess.check_output(['swaymsg', f"[con_id=\"{window_id}\"]", 'focus'])

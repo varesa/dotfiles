c83e1e62556ee847235e35c7f3a530ca7b7848ac
Esa Varemo <esa@kuivanto.fi>
2019-06-23 00:27:33 +0300

    Changes to alt-tab switcher

--- .config/sway/switch_window.py
+++ .config/sway/switch_window.py
@@ -6,7 +6,6 @@ import json
 tree = json.loads(subprocess.check_output(['swaymsg', '-t', 'get_tree']))
 
 def handle_container(con):
-    #print(con['name'])
 
     windows = []
 
@@ -15,7 +14,6 @@ def handle_container(con):
         app_name = con['app_id'] if con['app_id'] else con['window_properties']['class']
         app_title = con['name']
 
-        #print(f"<{app_id}> {app_name} --- {app_title}")
         windows.append((app_id, app_name, app_title,))
 
     for child in con['nodes']:
@@ -35,8 +33,7 @@ for output in tree['nodes']:
 
 windows_string = '\n'.join([f"<{app_id}> {app_name} --- {app_title}" for app_id, app_name, app_title in windows])
 
-#print(windows_string)
 
-selection = subprocess.check_output(['dmenu', '-i'], input=windows_string, universal_newlines=True)
+selection = subprocess.check_output(['rofi', '-dmenu', '-i'], input=windows_string, universal_newlines=True)
 window_id = selection.split(' ')[0][1:-1]
 subprocess.check_output(['swaymsg', f"[con_id=\"{window_id}\"]", 'focus'])

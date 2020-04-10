78b3c02a435db879c70eeb486c9d30771d485497
Esa Varemo <esa@kuivanto.fi>
2020-04-11 02:26:41 +0300

    Misc sway related files

---
+++ .config/sway/fix_workspaces2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+workspaces=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | select(.type == "workspace").name' | grep -E '[0-9]{2}')
+
+for workspace in $workspaces; do
+    workspace_num=$(echo $workspace | cut -c 2)
+    swaymsg workspace $workspace
+    sleep 0.1
+
+    output_num="$(./get_active_display.sh)"
+    swaymsg rename workspace to tmp_$output_num$workspace_num
+done
+
+workspaces=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | select(.type == "workspace").name' | grep -E '[0-9]{2}')
+for workspace in $workspaces; do
+    swaymsg workspace $workspace
+    sleep 0.1
+    swaymsg rename workspace to "$(echo $workspace | sed 's/tmp_//')"
+done
--- .config/sway/launch_apps.sh
+++ .config/sway/launch_apps.sh
@@ -2,4 +2,5 @@
 
 active_display=$(~/.config/sway/get_active_display.sh)
 
-rofi -i -modi drun -show drun -m XWAYLAND$active_display
+#rofi -i -modi drun -show drun -m XWAYLAND$active_display
+wofi -i --show drun

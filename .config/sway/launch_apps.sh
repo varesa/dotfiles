274d92e240d005aeec66654027c3b333bb735054
Esa Varemo <esa.varemo@cybercom.com>
2021-01-05 22:32:19 +0200

    Launch apps in cgroups/systemd scopes

--- .config/sway/launch_apps.sh
+++ .config/sway/launch_apps.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-active_display=$(~/.config/sway/get_active_display.sh)
+DIR="$HOME/.config/sway"
 
+cmd="$(wofi -i --show drun --conf ${DIR}/wofi-drun-nolaunch.conf | sed 's/ \?%.//g' | tr -d '\n')"
+swaymsg exec ${DIR}/launch_cgroup.sh "${cmd}"
+
+# Legacy:
+#active_display=$(~/.config/sway/get_active_display.sh)
 #rofi -i -modi drun -show drun -m XWAYLAND$active_display
-wofi -i --show drun
+#wofi -i --show drun

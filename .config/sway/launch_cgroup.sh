e43534177edde56afcaec835dd3a086c8d9f7ec2
Esa Varemo <esa.varemo@cybercom.com>
2023-11-27 15:47:51 +0200

    Limit memory usage per app

--- .config/sway/launch_cgroup.sh
+++ .config/sway/launch_cgroup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# MemTotal in KB -> *1000 to bytes -> /100 to percents
+# *1000/100 = *10
+mem_percent="$(($(awk '/MemTotal/ {print $2}' /proc/meminfo) * 10))"
+
+mem_high="$(($mem_percent * 50))"
+mem_max="$(($mem_percent * 75))"
+
 app_cleaned="$(echo $1 | sed 's/[^a-zA-Z]\+/_/g')"
-exec systemd-run --user --scope --slice=app.slice --unit="app-sway-${app_cleaned}-$$-${RANDOM}" "$@"
+
+exec systemd-run \
+    --user --scope \
+    --slice=app.slice --unit="app-sway-${app_cleaned}-$$-${RANDOM}" \
+    -p MemoryHigh="$mem_high" -p MemoryMax="$mem_max" \
+    "$@"
